49cc95ad3e3796d7f34f5f53495cebd4bb3fe95b
Crisiroid <csiroid@gmail.com>
2022-02-18 00:18:07 +0330

    managind user inputs

--- src/main/java/input.java
+++ src/main/java/input.java
@@ -6,7 +6,13 @@ public class input {
         Scanner scanner = new Scanner(System.in);
         System.out.println("PLease enter your name: ");
         String name = scanner.nextLine();
+        System.out.println("Please enter your age; ");
+        int age = scanner.nextInt();
+        //clering scanner to avoid bugs
+        scanner.nextLine();
+        System.out.println("What is your favourite food? ");
+        String food = scanner.nextLine();
 
-        System.out.println("Hello " + name);
+        System.out.println("Hello " + name + " You have " + age + " Years old!" + "\n and your favourite food is "+ food);
     }
 }
