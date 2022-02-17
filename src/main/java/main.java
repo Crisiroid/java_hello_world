40cb85f6b5ad478e60cdc1411d2ab6fa5ef72de8
Crisiroid <csiroid@gmail.com>
2022-02-17 23:55:27 +0330

    this is my second update

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,12 @@
 public class main {
     public static void main(String[] args){
-        System.out.println("Hello Java");
+        int h = 2;
+        long w = 12312421315214l;
+        float f = 1.23124213f;
+        String x = "water";
+        String y = "kool-aid";
+        y = x;
+        System.out.println("y: " + y);
+        System.out.println("x: " + x);
     }
 }
